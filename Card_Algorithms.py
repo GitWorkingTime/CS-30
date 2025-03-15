da339418da790a4a75b213bfb70a3d8ced9f80b5
GitWorkingTime <113561439+GitWorkingTime@users.noreply.github.com>
2025-03-15 11:22:41 -0600

    During school

--- Card_Algorithms.py
+++ Card_Algorithms.py
@@ -1,15 +1,38 @@
 import random
 import copy
 
-value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-suite = ['♥', '♠', '♦', '♣' ]
-set = [] 
+value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ]
+suite = ['♥', '♠', '♦', '♣']
+set = []
 
 def set_value():
-    valueInd = random.randint(1, 13) - 1
-    suiteInd = random.randint(1, 4) - 1
+    valueInd = random.randint(0, 11)
+    suiteInd = random.randint(0, 3)
+    #print(f"Value ind: {valueInd} and Suite Ind: {suiteInd}.")
     card = f"{suite[suiteInd]} {value[valueInd]}"
     return card
+
+def duplicate_check(hand):
+    length = len(hand)
+    for i in range(length):
+        for j in range(i):
+            if hand[i] == hand[j]:
+                while hand[j] == hand[i]:
+                    hand[i] = set_value()
+    return hand
+
+def convert_face_to_value(hand):
+    handCopy = copy.copy(hand)
+    for i in range(len(handCopy)):
+        if handCopy[i] == 'A':
+            handCopy[i] = 1
+        elif handCopy[i] == 'J':
+            handCopy[i] = 11
+        elif handCopy[i] == 'Q':
+            handCopy[i] = 12
+        elif handCopy[i] == 'K':
+            handCopy[i] = 13
+    return handCopy
 
 def create_hand():
     length = random.randint(1, 13)
@@ -25,38 +48,21 @@ def create_set():
         set.append(create_hand())
     return set
 
-def duplicate_check(hand):
-    for i in range(len(hand)):
-        for j in range(i):
-            if hand[i] == hand[j]:
-                while hand[j] == hand[i]:
-                    hand[i] = set_value()
-    return hand
-
 def check_equal(hand):
     length = len(hand)
     handCopy = copy.copy(hand)
-    if length < 1: #If there is no card
-        return False 
-    if length == 1: #If there is only one card
-        return True
-    if length > 4: #If there is more than 4 cards
+    
+    if length < 1 and length > 4:
         return False
+    elif length == 1:
+        return True
+    
     
     for i in range(length):
-        handCopy[i] = hand[i][2:]
-
-        if handCopy[i] == 'A':
-            handCopy[i] = 1
-        elif handCopy[i] == 'J':
-            handCopy[i] = 11
-        elif handCopy[i] == 'Q':
-            handCopy[i] = 12
-        elif handCopy[i] == 'K':
-            handCopy[i] = 13
-    
-    for i in range(length):
+        handCopy[i] = handCopy[i][2:]
+        handCopy = convert_face_to_value(handCopy)
         handCopy[i] = int(handCopy[i])
+    
     if handCopy[1:] == handCopy[:-1]:
         return True
     else:
@@ -71,11 +77,65 @@ def shuffle(hand):
     print(hand)
     return hand
 
-def three_of_a_kind():
-    pass
+def three_of_a_kind(hand):
+    handCopy = copy.copy(hand)
+    print(handCopy)
+    valueRepeats = list(range(13))
+    valueInt = copy.copy(value)
+    valueInt = convert_face_to_value(valueInt)
+    for i in range(13):
+        valueRepeats[i] = 0
+    
+    length = len(handCopy)
+    
+    if length < 3:
+        return False
+    
+    for i in range(length):
+        handCopy[i] = handCopy[i][2:]
+    handCopy = convert_face_to_value(handCopy)
+    
+    for i in range(length):
+        for j in range(13):
+            if handCopy[i] == valueInt[j]:
+                valueRepeats[j] += 1
+    
+    # for i in range(13):
+    #     print(f"{value[i]}: {valueRepeats[i]}")
+    
+    for i in range(13):
+        if valueRepeats[i] == 3:
+            return True
+    return False
 
 def num_of_pairs():
+    pairRepeats = 0
+    handCopy = copy.copy(hand)
+    print(handCopy)
+    valueRepeats = list(range(13))
+    valueInt = copy.copy(value)
+    valueInt = convert_face_to_value(valueInt)
+    for i in range(13):
+        valueRepeats[i] = 0
+    
+    length = len(handCopy)
+    
+    for i in range(length):
+        handCopy[i] = handCopy[i][2:]
+    handCopy = convert_face_to_value(handCopy)
+    
+    for i in range(length):
+        for j in range(13):
+            if handCopy[i] == valueInt[j]:
+                valueRepeats[j] += 1
+    
+    for i in range(13):
+        print(f"{value[i]}: {valueRepeats[i]}")
     pass
 
 def highest_pair():
     pass
+
+print(three_of_a_kind(create_hand()))
+
+
