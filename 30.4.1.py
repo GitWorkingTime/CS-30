fa15d6e71ec8ee63079fb76b39b924be4d6f0beb
GitWorkingTime <113561439+GitWorkingTime@users.noreply.github.com>
2025-03-18 18:20:14 +0000

    Started on 30.4.1

---
+++ 30.4.1.py
@@ -0,0 +1,23 @@
+class Car():
+    pass
+
+class Electronics():
+    pass
+
+class Animal():
+    pass
+
+lamborghini = Car()
+playstation = Electronics()
+cat = Animal()
+
+lamborghini.colour = "red"
+lamborghini.horsepower = 40
+
+playstation.ram = "32GB"
+playstation.storage = "5TB"
+
+cat.breed = "Siamese"
+cat.eyeColour = "Brown"
+
+print(playstation.ram)
