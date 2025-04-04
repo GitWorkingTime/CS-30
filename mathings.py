3e8d0a04b29f1a85178f4efa10f8fd1bd4ce18fc
GitWorkingTime <113561439+GitWorkingTime@users.noreply.github.com>
2025-04-04 17:19:13 +0000

    division and pythagoras

--- mathings.py
+++ mathings.py
@@ -14,7 +14,7 @@ class Mathings:
         return x * y
 
     @staticmethod
-    def dividision(x, y):
+    def division(x, y):
         return round(x / y, 5)
     
     @staticmethod
@@ -176,10 +176,34 @@ class Main():
             
             num2 = int(num2)
 
-            result = Mathings.dividision(num1, num2)
-            print(f"The sum is: {result}")
+            result = Mathings.division(num1, num2)
+            print(f"The quotient is: {result}")
             pass
         case '4':
+            print("Choose first number:")
+            num1 = input()
+            num1 = cleanUpInput(num1)
+            if validate_input(num1) == False:
+                while validate_input(num1) == False:
+                    print("Try again!")
+                    num1 = input()
+                    num1 = cleanUpInput(num1)
+                
+            num1 = int(num1)
+
+            print("Choose second number:")
+            num2 = input()
+            num2 = cleanUpInput(num2)
+            if validate_input(num2) == False:
+                while validate_input(num2) == False:
+                    print("Try again!")
+                    num2 = input()
+                    num2 = cleanUpInput(num2)
+            
+            num2 = int(num2)
+
+            result = Mathings.pythagoras(num1, num2)
+            print(f"The hypotenuse is: {result}")
             pass
         case '5':
             pass
